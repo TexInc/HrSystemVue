bc6b566c40a279e0ce0e3dd6947412fdddabd508
Loyio <i@loyio.me>
2020-11-16 13:08:33 +0800

    fulfill the component

--- src/main.js
+++ src/main.js
@@ -3,13 +3,47 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import store from './store'
+import {getRequest, postRequest, deleteRequest, putRequest} from './utils/api'
+
+import {initMenu, isNotNullORBlank} from './utils/utils'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+
+Vue.prototype.getRequest = getRequest
+Vue.prototype.postRequest = postRequest
+Vue.prototype.deleteRequest = deleteRequest
+Vue.prototype.putRequest = putRequest
+Vue.prototype.isNotNullORBlank = isNotNullORBlank
+
+router.beforeEach((to, from, next) => {
+  if (to.name == 'Login') {
+    next()
+    return
+  }
+  var name = store.state.user.name
+  if (name == '未登录') {
+    if (to.meta.requireAuth || to.name == null) {
+      next({path: '/', query: {redirect: to.path}})
+    } else {
+      next()
+    }
+  } else {
+    initMenu(router, store)
+    if (to.path == '/chat') { store.commit('updateMsgList', []) }
+    next()
+  }
+}
+)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
